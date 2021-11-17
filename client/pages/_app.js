c94b3f102c1842c437745e2e78834435cebbc6a0
AbhinavXT <abhinavpathaka17@gmail.com>
2021-11-17 19:27:40 +0530

    ChainID

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,7 +3,26 @@ import Link from 'next/link'
 import { useState, useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
-	const [account, setAccount] = useState('')
+	const [currentAccount, setCurrentAccount] = useState('')
+	const [correctNetwork, setCorrectNetwork] = useState(false)
+
+	const checkIfWalletIsConnected = async () => {
+		const { ethereum } = window
+		if (ethereum) {
+			console.log('Got the ethereum obejct: ', ethereum)
+		} else {
+			console.log('Connect your wallet first!')
+		}
+
+		const accounts = await ethereum.request({ method: 'eth_accounts' })
+
+		if (accounts.length !== 0) {
+			console.log('Found authorized Account: ', accounts[0])
+			setCurrentAccount(accounts[0])
+		} else {
+			console.log('No authorized account found')
+		}
+	}
 
 	const connectWallet = async () => {
 		try {
@@ -13,38 +32,75 @@ function MyApp({ Component, pageProps }) {
 				console.log('Metamask not detected')
 				return
 			}
+			let chainId = await ethereum.request({ method: 'eth_chainId' })
+			console.log('Connected to chain:' + chainId)
+			const rinkebyChainId = '0x4'
+			if (chainId !== rinkebyChainId) {
+				alert('You are not connected to the Rinkeby Testnet!')
+				return
+			}
 
 			const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
 
 			console.log('Found account', accounts[0])
-			setAccount(accounts[0])
+			setCurrentAccount(accounts[0])
 		} catch (error) {
 			console.log('Error connecting to metamask', error)
 		}
 	}
 
+	const checkCorrectNetwork = async () => {
+		const { ethereum } = window
+		let chainId = await ethereum.request({ method: 'eth_chainId' })
+		console.log('Connected to chain:' + chainId)
+		const rinkebyChainId = '0x4'
+		if (chainId !== rinkebyChainId) {
+			setCorrectNetwork(false)
+		} else {
+			setCorrectNetwork(true)
+		}
+	}
+
+	useEffect(() => {
+		checkIfWalletIsConnected()
+		checkCorrectNetwork()
+	}, [])
+
 	return (
 		<div className='bg-gray-300 h-full min-h-screen'>
-			{account === '' ? (
-				<div className='flex justify-center pt-20'>
-					<button
-						className='text-2xl font-bold py-3 px-12 bg-gray-500 hover:text-gray-700 hover:bg-gray-400 transition-colors duration-300 shadow-lg rounded-lg mb-10'
-						onClick={connectWallet}
-					>
-						Connect Wallet
-					</button>
-				</div>
-			) : (
-				<div>
-					<div className='flex justify-between items-baseline pt-8 px-20'>
-						<div className='font-extrabold text-3xl'>Eternal Domain</div>
-						<div className='flex gap-x-12 text-xl font-bold'>
-							<Link href='/'>Home</Link>
-							<Link href='/mint'>Mint</Link>
-							<Link href='/mynft'>My NFT</Link>
+			{correctNetwork &&
+				(currentAccount === '' ? (
+					<div className='flex flex-col justify-center items-center pt-20 gap-y-10'>
+						<div className='text-3xl font-extrabold'>
+							Welcome to Eternal Domain
 						</div>
+						<button
+							className='text-2xl font-bold py-3 px-12 bg-gray-500 hover:bg-gray-400 transition-colors duration-300 shadow-lg rounded-lg mb-10'
+							onClick={connectWallet}
+						>
+							Connect Wallet
+						</button>
 					</div>
-					<Component {...pageProps} />
+				) : (
+					<div>
+						<div className='flex justify-between items-baseline pt-8 px-20'>
+							<div className='font-extrabold text-3xl'>Eternal Domain</div>
+							<div className='flex gap-x-12 text-xl font-bold'>
+								<Link href='/'>Home</Link>
+								<Link href='/mint'>Mint</Link>
+								<Link href='/mynft'>My NFT</Link>
+							</div>
+						</div>
+						<Component {...pageProps} />
+					</div>
+				))}
+			{!correctNetwork && (
+				<div className='flex flex-col justify-center items-center pt-40 font-bold text-2xl gap-y-3'>
+					<div>----------------------------------------</div>
+					<div>Wrong Network</div>
+					<div>Please connect to the Rinkeby Testnet</div>
+					<div>and reload the page</div>
+					<div>----------------------------------------</div>
 				</div>
 			)}
 		</div>
